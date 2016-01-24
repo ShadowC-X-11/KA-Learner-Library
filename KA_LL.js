7fe382db3956e7a2502a59b33c78b29ed4f81d3b
ShadowCX11 <shadowcx11sb@gmail.com>
2016-01-23 23:10:59 -0400

    Add rsaEncrypt, rsaDecrypt, isPrime, generatePrime, modinv, gcd

--- KA_LL.js
+++ KA_LL.js
@@ -79,7 +79,114 @@ var KA_LL = {
 				return 1 * ordering;
 			}
 		};
-		return theFunction;    
+		return theFunction;
+	},
+	modinv: function(a, n){
+		// Taken from https://en.wikipedia.org/wiki/RSA_(cryptosystem)#Code
+		// Calculates the modular multiplicative inverse
+		// Added for the RSA functions
+    	var t  = 0,
+            nt = 1,
+            r  = n,
+            nr = a % n;
+        if (n < 0){
+        	n = -n;
+        }
+        if (a < 0){
+        	a = n - (-a % n);
+        }
+    	while (nr !== 0) {
+    		var quot= (r/nr) | 0;
+    		var tmp = nt;  nt = t - quot*nt;  t = tmp;
+    		    tmp = nr;  nr = r - quot*nr;  r = tmp;
+    	}
+    	if (r > 1) { return -1; }
+    	if (t < 0) { t += n; }
+    	return t;
+    },
+	isPrime: function(number){
+		// Checks if a number is prime
+		for(var i = 0; i <= floor(Math.sqrt(number)); i++){
+			if(number % i === 0){
+				return false;
+			}
+		}
+		return true;
+	},
+	generatePrime: function(min, max){
+		// generates a random prime number
+		while(true){
+			var rnd = Math.floor(Math.random() * ((max - 1) - min + 1)) + min;
+			var s = Math.floor(Math.sqrt(rnd));
+			for(var i = 2; i <= s; i++){
+				if(rnd % i === 0){
+					continue;
+				}
+			}
+			return rnd;
+		}
+	},
+	gcd: function(num1, num2){
+		// Calculates the Greatest Common Denominator
+		var b = num1 > num2 ? num2 : num1;
+		var gcd = 0;
+		for(var i = 1; i <= b; i++){
+			if(num1 % i === 0 && num2 % i === 0){
+				gcd = i;
+			}
+		}
+		return gcd;
+	},
+	rsaEncrypt: function(text, ek){
+		// Generates RSA encrypted data
+		var returned = {};
+		if(typeof ek !== "string"){
+			var p = KA_LL.randomPrime(1, 1000);
+			var q = KA_LL.randomPrime(1, 1000);
+			var m = p * q;
+			var phim = m - (p + q - 1);
+			var en = m - 1;
+			while(KA_LL.gcd(en, m) !== 1){
+				var en = randomPrime(1, phim);
+			}
+			var dn = KA_LL.modinv(en, phim);
+			returned.privKey = dn.toString() + "," + m.toString();
+			ek = returned.publKey = en.toString() + "," + m.toString();
+		}
+		var data = ek.split(",");
+		var ek = parseInt(data[0]);
+		var m = parseInt(data[1]);
+		var encMess = "";
+		var allowed = "a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z \\! \\? \\. , @ # \\$ % & \\:\\/ \\\\ \\< \\> \\[ \\] \\{ \\} ~ \\^ \\( \\) \\- \\_ \\=".split(" ");
+		allowed.push(" ");
+		for(var i = 0; i < text.length; i++){
+			if(i !== 0) encMess += ", ";
+			var tmp = allowed.indexOf(text[i]);
+			if(tmp === -1){
+				tmp = allowed.indexOf("?");
+			}
+			tmp = Math.pow(tmp, ek) % m;
+			encMess += tmp;
+		}
+		returned.encrptdMsg = encMess;
+		return returned;
+	},
+	rsaDecrypt: function(encMsg, pk){
+		// decrypts RSA encrypted data
+		if(typeof pk !== "string"){
+			return null;
+		}
+		var data = pk.split(",");
+		var dk = parseInt(data[0]);
+		var m = parseInt(data[1]);
+		var alphalist = "a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z \\! \\? \\. , @ # \\$ % & \\:\\/ \\\\ \\< \\> \\[ \\] \\{ \\} ~ \\^ \\( \\) \\- \\_ \\=".split(" ");
+		alphalist.push(" ");
+		var msgvals = encMsg.split(" ");
+		var out = "";
+		for(var i = 0; i < msgvals.length; i++){
+			out += alphalist[Math.pow(parseInt(msgvals[i]), dk) % m];
+		}
+		return out;
 	}
 };
 
